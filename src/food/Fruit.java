04adeae538449c64210fb49d50c058a1995546f0
Vuyo_M_ Buthelezi <vuyanimondlibuthelezi@gmail.com>
2021-11-02 12:49:07 +0200

    first commit

---
+++ src/food/Fruit.java
@@ -0,0 +1,7 @@
+package food;
+
+public abstract class Fruit {
+    private double weight;
+    private int yearProduced;
+    public abstract void price();
+}
---
+++ src/interfaces/TestIF.java
@@ -0,0 +1,15 @@
+package interfaces;
+
+public class TestIF implements StaticIface {
+    public static void main(String[] args) {
+        System.out.println(StaticIface.m1());
+
+        new TestIF().go();
+//        System.out.println(m1());
+
+    }
+
+    void go() {
+        System.out.println(StaticIface.m1());
+    }
+}
