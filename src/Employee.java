caf12bae04386bab0eb8f8436c2589392d5d33c1
Vuyo_M_ Buthelezi <vuyanimondlibuthelezi@gmail.com>
2021-11-11 09:53:58 +0200

    next test

---
+++ src/Employee.java
@@ -0,0 +1,22 @@
+/** I am a commnander
+ *
+ * // I kill humans
+ */
+
+// // vuyi
+
+public class Employee {
+    public float parseFloat(String s) {
+        float f = 0.0f;      // 1
+        try {
+            f = Float.valueOf(s).floatValue();    // 2
+//            return f;      // 3
+        } catch (NumberFormatException nfe) {
+            f = Float.NaN;    // 4
+//            return f;     // 5
+        } finally {
+            return f;     // 6
+        }
+//        return f;    // 7
+    }
+}
